688025bd218618464598ce31fbd5ff78c984f45a
Dziyana.Sialiuk <dianaselyuk02@mail.ru>
2025-05-20 10:38:27 +0300

    feat: create board routes

---
+++ server/src/routes/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import bodyParser from "body-parser";
+import {
+  createBoard,
+  deleteBoard,
+  getAllBoards,
+  updateBoard,
+} from "../controllers/boardController";
+
+const router = express.Router();
+
+router.get("/api/getAllBoards", getAllBoards);
+router.post("/api/createBoard", express.json(), createBoard);
+router.patch("/api/updateBoard/:id", bodyParser.json(), updateBoard);
+router.delete("/api/deleteBoard/:id", deleteBoard);
+
+export default router;
